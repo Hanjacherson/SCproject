51c599278092f8028ebf5e3fbf089e1dd47efa5c
0yHan <dygks0202@naver.com>
2023-11-22 12:25:13 +0900

    파형 분석

--- iot/record.py
+++ iot/record.py
@@ -1,10 +1,25 @@
+import numpy as np
 import librosa
-import librossa.display
+import librosa.display
 import matplotlib.pyplot as plt
 
-f = 'output.wav'
-y, sh = librosa.load(f)
+# 오디오 파일 로드
+file_path = 'output.wav'  # 대체할 오디오 파일 경로
+# 오디오 파일의 일부만 로드
+y, sr = librosa.load(file_path, sr=22050, offset=0.0, duration=30.0)  # 처음 30초만 로드
 
-plt.figure(figsize(12,4))
-librosa.display.waveshow(y, sr=sr)
+# 멜 스펙트로그램 생성
+S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+log_S = librosa.power_to_db(S, ref=np.max)
+
+# 멜 스펙트로그램 플롯
+plt.figure()
+librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
+plt.title('Mel Spectrogram')
+plt.colorbar(format='%+02.0f dB')
+plt.tight_layout()
 plt.show()
+
+# 템포 추정
+tempo, _ = librosa.beat.beat_track(y, sr=sr)
+print(f"Estimated Tempo: {tempo} BPM")
