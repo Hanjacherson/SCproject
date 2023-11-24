a86aecfb5605357eb67d34ec561789d27718922f
0yHan <dygks0202@naver.com>
2023-11-24 17:49:07 +0900

    please..

--- iot/transferSoundData.py
+++ iot/transferSoundData.py
@@ -5,18 +5,20 @@ import time
 
 app = Flask(__name__)
 
-def send_file_to_spring():
+def send_file_to_spring(filename):
     while True:
         try:
-            response = requests.get('http://192.168.21.152:5050')
-            print(f"File sent. Response status code: {response.status_code}")
+            with open(filename, 'rb') as f:
+                files = {'file': f}
+                response = requests.post('http://192.168.21.152:5021/file', files=files)
+                print(f"File sent. Response status code: {response.status_code}")
         except Exception as e:
             print(f"An error occurred: {e}")
         time.sleep(10)  # 10초 대기
 
 @app.before_first_request
-def activate_job():
-    thread = threading.Thread(target=send_file_to_spring)
+def activate_job(filename):
+    thread = threading.Thread(target=send_file_to_spring(filename))
     thread.daemon = True  # 주 스레드가 종료될 때 백그라운드 스레드도 종료되도록 설정
     thread.start()
 
@@ -25,4 +27,4 @@ def index():
     return "Flask is running!"
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5055)
+    app.run("0.0.0.0", port=5051)
