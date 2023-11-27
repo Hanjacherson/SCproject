622212e7cae4feec2dde46bcc1da9517fe5129bb
0yHan <dygks0202@naver.com>
2023-11-27 15:10:08 +0900

    iot랑 app연결

--- iot/notification.py
+++ iot/notification.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from http import HTTPStatus
+from flask import Flask, request, jsonify
 import requests
 import logging
 
@@ -11,14 +12,14 @@ ifttt_webhook_url = 'https://maker.ifttt.com/trigger/dog_check_please/with/key/d
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Flask 라우트 설정
-@app.route('/send_value', methods=['POST'])
-def send_value():
-    # POST 요청으로 전송된 값 가져오기
-    data = request.get_json()
-
+@app.route('/data', methods=['GET','POST'])
+def receive_data():
+    data = request.json
+    # data = request.get_json() post
+    print("Received data:", data)
+    
     # 값이 2 이상인 경우에만 IFTTT로 알림 보내기
-    if 'value' in data and data['value'] >= 2:
+    if 'value' in data and data['value'] == 1:
         send_notification()
         return 'Notification sent'
     else:
@@ -26,7 +27,7 @@ def send_value():
 
 def send_notification():
     # IFTTT로 POST 요청 보내기
-    data1 = {'value1': '노드조'}
+    data1 = {'값1': '노드조1'}
     try:
         response = requests.post(ifttt_webhook_url, json=data1)
         response.raise_for_status()  # 나쁜 응답에 대해 HTTPError 발생
@@ -36,6 +37,5 @@ def send_notification():
     except requests.exceptions.RequestException as err:
         logger.error(f"요청 예외: {err}")
 
-if __name__ == '__main__':
-    # 서버 실행
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5080)
