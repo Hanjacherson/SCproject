0dfd96101ab28e0e409fdbbb98874c9c9c750c4c
Hanjateacherson <74088300+Hanjateacherson@users.noreply.github.com>
2023-11-28 18:20:51 +0900

    Update home.js
    랜덤 데이터를 삭제

--- WeirdKids/static/js/home.js
+++ WeirdKids/static/js/home.js
@@ -27,12 +27,6 @@ function openSideNav() {
 function closeSideNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-
-// Redirect to the profile page
-function redirectToProfilePage() {
-    window.location.href = '/profile';
-}
-
 // Add this script to your existing script
 document.addEventListener('DOMContentLoaded', function () {
     // Get all elements with class 'timeline-content'
@@ -71,43 +65,41 @@ function generateDateLabels() {
     return dateLabels;
 }
 
-// 데이터 생성
-var dataset1 = generateRandomData();
-var dataset2 = generateRandomData();
-
-// 차트 생성
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: generateDateLabels(), // 날짜 레이블 추가
-        datasets: [
-            {
-                label: 'Whimmping',
-                backgroundColor: '#FF5712',
-                data: dataset1
+// 서버에서 MySQL 데이터 가져오기
+fetch("/")
+    .then(response => response.json())
+    .then(data => {
+        // 데이터 처리 및 차트 생성
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: generateDateLabels(), // 날짜 레이블 추가
+                datasets: [
+                    {
+                        label: 'Whimmping',
+                        backgroundColor: '#FF5712',
+                        data: data.map(item => item.count) // MySQL 데이터에서 count 값 가져오기
+                    },
+                    // 추가적인 데이터셋을 원하면 여기에 추가
+                ]
             },
-            {
-                label: 'Barking',
-                backgroundColor: 'rgba(255, 87, 18, 0.5)',
-                data: dataset2
-            }
-        ]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-                max: 10,
-                ticks: {
-                    stepSize: 5
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        max: 10,
+                        ticks: {
+                            stepSize: 5
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'top'
+                    }
                 }
             }
-        },
-        plugins: {
-            legend: {
-                position: 'top'
-            }
-        }
-    }
-});
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
