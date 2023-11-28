540961abf928094f5dfccc34167e8e126e625cac
0yHan <dygks0202@naver.com>
2023-11-28 10:17:31 +0900

    server.py에 app알람 기능 추가

--- WeirdKids/server.py
+++ WeirdKids/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, session, render_template, redirect, request, url_for, flash, jsonify
 from datetime import datetime
 import pymysql
+import requests
+import logging
+from http import HTTPStatus
 
 mysql = pymysql.connect(host="project-db-stu3.smhrd.com", 
                        db="Insa4_IOTB_final_4", 
@@ -11,6 +14,13 @@ mysql = pymysql.connect(host="project-db-stu3.smhrd.com",
 
 app = Flask(__name__)
 app.secret_key = "12345"
+
+# IFTTT 웹훅 URL
+ifttt_webhook_url = 'https://maker.ifttt.com/trigger/dog_check_please/with/key/d4BLCPHNfnxfSnep3Vl4dw'
+
+# 로깅 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app.static_url_path = '/static'
 app.static_folder = 'static'
@@ -271,6 +281,30 @@ def policy():
 def termsofuse():
     return render_template('termsofuse.html')
 
+@app.route('/data', methods=['GET','POST'])
+def receive_data():
+    data = request.json
+    # data = request.get_json() post
+    print("Received data:", data)
+    
+    # 값이 2 이상인 경우에만 IFTTT로 알림 보내기
+    if 'value' in data and data['value'] == 1:
+        send_notification()
+        return 'Notification sent'
+    else:
+        return 'Value is less than 2, no notification sent'
+
+def send_notification():
+    # IFTTT로 POST 요청 보내기
+    data1 = {'값1': '노드조1'}
+    try:
+        response = requests.post(ifttt_webhook_url, json=data1)
+        response.raise_for_status()  # 나쁜 응답에 대해 HTTPError 발생
+        logger.info("알림이 성공적으로 전송되었습니다.")
+    except requests.exceptions.HTTPError as errh:
+        logger.error(f"HTTP 오류: {errh}")
+    except requests.exceptions.RequestException as err:
+        logger.error(f"요청 예외: {err}")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
