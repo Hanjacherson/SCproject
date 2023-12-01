93412c07d61363b933fa7da80b0213951d82664d
Hanjateacherson <74088300+Hanjateacherson@users.noreply.github.com>
2023-12-01 10:07:26 +0900

    Add files via upload
    일별 누적 데이터 추가

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask, session, render_template, redirect, request, url_for, flash, jsonify, g
-from datetime import datetime, timedelta
-from werkzeug.utils import secure_filename
-from werkzeug.security import check_password_hash
-import pymysql, os, sqlite3
-from flask_cors import CORS
+from flask import Flask, session, render_template, redirect, request, url_for, flash, jsonify
+from datetime import datetime
+import pymysql
+import requests
+import logging
+from http import HTTPStatus
 
 mysql = pymysql.connect(host="project-db-stu3.smhrd.com", 
                        db="Insa4_IOTB_final_4", 
@@ -14,6 +14,13 @@ mysql = pymysql.connect(host="project-db-stu3.smhrd.com",
 
 app = Flask(__name__)
 app.secret_key = "12345"
+
+# IFTTT 웹훅 URL
+ifttt_webhook_url = 'https://maker.ifttt.com/trigger/dog_check_please/with/key/d4BLCPHNfnxfSnep3Vl4dw'
+
+# 로깅 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app.static_url_path = '/static'
 app.static_folder = 'static'
@@ -32,11 +39,6 @@ def DML(sql, params=None):
     cursor.close()
     return "success!!"
 
-@app.route('/')
-def index():
-    # /login으로 리다이렉트
-    return redirect(url_for('login'))
-
 @app.route('/home/', methods=['GET', 'POST'])
 def home():
     error = None
@@ -46,11 +48,36 @@ def home():
 
     user_id = session['login_user']
 
-   # 펫 정보 및 보호자 닉네임 가져오기
+    # 펫 정보 및 보호자 닉네임 가져오기
     pet_info, guardian_nickname = get_pet_and_guardian_info(user_id)
-    
-    return render_template('home.html', error=error, user_id=user_id, pet_info=pet_info, guardian_nickname=guardian_nickname)
 
+    # Connect to MySQL
+    mysql = pymysql.connect(
+        host="project-db-stu3.smhrd.com",
+        db="Insa4_IOTB_final_4",
+        user="Insa4_IOTB_final_4",
+        password="aischool4",
+        port=3307,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    # Create a cursor object
+    cursor = mysql.cursor()
+
+    # Execute your SQL query
+    cursor.execute("SELECT pet_idx, t_voice.result_at, "
+                   "CASE WHEN t_voice.voice_result = 0 THEN 'Barking' ELSE 'Whimpping' END AS voice_result "
+                   "FROM t_voice")
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Close the cursor and connection
+    cursor.close()
+    mysql.close()
+
+    # Pass the data to the template
+    return render_template('home.html', error=error, user_id=user_id, pet_info=pet_info, guardian_nickname=guardian_nickname, results=results)
 
 def get_pet_and_guardian_info(user_id):
     # 가상의 코드: 사용자의 강아지 정보와 보호자 닉네임을 데이터베이스에서 가져온다고 가정
@@ -78,140 +105,50 @@ def get_pet_and_guardian_info(user_id):
 
     return pet_info, guardian_nickname
 
-def get_pet_info(user_id):
-    # 가상의 코드: 사용자의 강아지 정보를 데이터베이스에서 가져온다고 가정
-    sql = "SELECT pet_idx, pet_name, created_at FROM t_pet JOIN t_member USING (id) WHERE t_pet.id = %s"
-    result = DQL(sql, (user_id,))
-
-    # 결과가 있다면 첫 번째 행을 사용
-    if result:
-        pet_info = result[0]
-        return pet_info
-
-    # 결과가 없다면 None 반환
-    return None
-
-def get_pet_idx(user_id):
-    # 가상의 코드: 사용자의 pet_idx 정보를 데이터베이스에서 가져온다고 가정
-    sql = "SELECT pet_idx FROM t_pet WHERE id = %s"
-    result = DQL(sql, (user_id,))
-
-    # 결과가 있다면 첫 번째 행의 pet_idx를 사용
-    if result:
-        pet_idx = result[0]['pet_idx']
-        return pet_idx
-
-    # 결과가 없다면 None 반환
-    return None
 
 
-@app.route('/get_pet_info', methods=['GET'])
-def get_pet_info_route():
-    member_id = request.args.get('member_id')
-    pet_id = request.args.get('id')
-
-    # 여기에서 적절한 SQL 쿼리를 사용하여 펫 정보를 가져오는 로직을 작성
-    # 이하 코드는 예시이며, 실제로 사용하는 테이블 및 필드명에 맞게 수정해야 합니다.
-    pet_sql = "SELECT pet_idx, pet_name, created_at, pet_img FROM t_pet JOIN t_member USING (id) WHERE t_pet.id = %s"
-    pet_result = DQL(pet_sql, (member_id, pet_id))
-
-    if pet_result:
-        pet_info = pet_result[0]
-        return jsonify(pet_info)
-    else:
-        return jsonify({'error': '반려견 정보를 찾을 수 없습니다'}), 404
-    
 
 
-# 업로드 폴더 설정
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/update_pet_info', methods=['POST'])
+def update_pet_info_route():
+    if 'login_user' not in session:
+        return redirect(url_for('login'))
 
-# 기본 이미지 파일명 설정
-DEFAULT_IMAGE = 'default_image.jpg'
+    user_id = session['login_user']
+    pet_name = request.form.get['pet_name']
+    pet_birth_year = request.form['pet_birth_year']
+    pet_birth_month = request.form['pet_birth_month']
+    pet_birth_day = request.form['pet_birth_day']
+    pet_submit_type = request.form.get('pet_submit_type')
 
+    # created_at 생성
+    created_at = datetime(int(pet_birth_year), int(pet_birth_month), int(pet_birth_day)).date()
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'pet_image' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
+    if pet_submit_type == '강아지 등록':
+        insert_pet_info(user_id, pet_name, created_at)
+    elif pet_submit_type == '강아지 정보 수정':
+        update_pet_info(user_id, pet_name, created_at)
 
-    file = request.files['pet_image']
+    return redirect(url_for('home'))
 
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-
-    if file:
-        filename = secure_filename(file.filename)
-        upload_path = os.path.join(app.root_path, 'static', 'uploads', filename)
-        file.save(upload_path)
-
-        # 업로드 성공 시에 파일 이름을 세션에 저장
-        session['uploaded_filename'] = filename
-
-        # 세션에서 사용자 ID 가져오기
-        user_id = session.get('login_user')
-        pet_id = session.get('current_pet_id')  # 현재 선택된 반려동물 ID
-
-        # 펫 정보 가져오기
-        pet_info = get_pet_info(user_id)
-
-        if user_id and pet_info:
-            # 사용자 ID를 통해 해당 사용자의 반려견 정보 업데이트
-            update_pet_info(user_id, pet_info['pet_name'], pet_info['created_at'], filename)  # filename이 이미지 파일명
-
-            flash('File successfully uploaded')
-            return redirect(url_for('home'))  # 업로드 완료 후 home으로 리디렉션
-        else:
-            flash('Invalid session data')
-
-    return render_template('profile.html')  # 업로드 실패 시 업로드 페이지로 리디렉션
-
-
-def update_pet_info(user_id, pet_name, created_at, pet_img=None):
+def insert_pet_info(user_id, pet_name, created_at):
+    update_pet_sql = "INSERT INTO t_pet (pet_name, created_at, id) VALUES (%s, %s, %s)"
     try:
-        # 사용자가 t_pet에 이미 레코드를 가지고 있는지 확인
-        check_query = "SELECT * FROM t_pet WHERE id = %s"
-        result = DQL(check_query, (user_id,))
-
-        if result:
-            # 이미 레코드가 있는 경우, 업데이트
-            update_query = "UPDATE t_pet SET pet_name=%s, created_at=%s, pet_img=%s WHERE id = %s"
-            DML(update_query, (pet_name, created_at, pet_img, user_id))
-        else:
-            # 레코드가 없는 경우, 삽입
-            insert_query = "INSERT INTO t_pet (pet_name, created_at, pet_img, id) VALUES (%s, %s, %s, %s)"
-            DML(insert_query, (pet_name, created_at, pet_img, user_id))
-
-        # 성공 메시지를 세션에 추가
-        session['update_success_message'] = True
-
+        DML(update_pet_sql, (pet_name, created_at, user_id))
+        flash('강아지 정보가 성공적으로 등록되었습니다.','success')
     except Exception as e:
-        print(f"Error in update_pet_info: {str(e)}")
-        # 실패 메시지를 세션에 추가
-        session['update_failure_message'] = True 
+        print("강아지 정보 등록 중 오류 발생:", str(e))
+        flash('강아지 정보 등록 중 오류가 발생했습니다.', 'error')
 
 
-@app.route('/show_pet_image/<filename>')
-def show_pet_image(filename):
-    # 세션에서 사용자 ID와 반려동물 ID 가져오기
-    member_id = session.get('login_user')  # 로그인한 사용자 ID
-    pet_id = session.get('current_pet_id')  # 현재 선택된 반려동물 ID
-
-    if member_id and pet_id:
-        # 데이터베이스에서 pet_img 필드를 검색합니다.
-        pet_img_sql = "SELECT pet_img FROM t_pet JOIN t_member USING (id) WHERE t_pet.id = %s"
-        pet_img_result = DQL(pet_img_sql, (member_id,))
-
-        if pet_img_result:
-            pet_img_filename = pet_img_result[0]['pet_img']
-            if pet_img_filename:
-                return f'<img src="{url_for("static", filename=f"uploads/{pet_img_filename}")}" alt="My Pet">'
-
-    # 이미지가 없으면 기본 이미지를 사용합니다.
-    return f'<img src="{url_for("static", filename=f"uploads/{DEFAULT_IMAGE}")}" alt="Default Pet Image">'
+def update_pet_info(user_id, pet_name, created_at):
+    update_pet_sql = "UPDATE t_pet SET pet_name = %s, created_at = %s WHERE id = %s"
+    try:
+        DML(update_pet_sql, (pet_name, created_at, user_id))
+        flash('강아지 정보가 성공적으로 수정되었습니다.','success')
+    except Exception as e:
+        print("강아지 정보 수정 중 오류 발생:", str(e))
+        flash('강아지 정보 수정 중 오류가 발생했습니다.', 'error')    
 
 
 
@@ -246,6 +183,7 @@ def signup():
         except Exception as e:
             print("Error during signup:", str(e))
   
+
     return render_template('signup.html')
 
 
@@ -278,8 +216,6 @@ def login():
         sql = "SELECT id FROM t_member WHERE id = %s AND pw = %s"
         user = DQL(sql, (id, pw))
 
-        
-
         if user:
             session['login_user'] = id
             return redirect(url_for('home'))  # 'home' 함수로 리디렉션
@@ -289,28 +225,17 @@ def login():
              # 세션에서 회원가입 성공 메시지를 가져와서 표시
     signup_success_message = session.pop('signup_success_message', None)
     
-
     return render_template('login.html', error=error, signup_success_message=signup_success_message)
 
-
-@app.route('/home/profile', methods=['GET', 'POST'])
+@app.route('/home/profile')
 def profile():
-
-    uploaded_filename = session.pop('uploaded_filename', None)
-
     # 로그인한 사용자의 ID를 가져옵니다.
     if 'login_user' not in session:
         return redirect(url_for('login'))
     user_id = session['login_user']
 
     # 사용자 정보를 데이터베이스에서 가져옵니다.
-    #sql = "SELECT id, email, nick, phone FROM t_member WHERE id = %s"
-    sql = """
-        SELECT t_member.id, email, nick, phone, created_at, pet_name
-        FROM t_member
-        LEFT JOIN t_pet ON t_member.id = t_pet.id
-        WHERE t_member.id = %s
-    """
+    sql = "SELECT id, email, nick, phone FROM t_member WHERE id = %s"
     user_info = DQL(sql, (user_id,))
 
     if not user_info:
@@ -321,34 +246,12 @@ def profile():
     user_info = user_info[0]  # 결과가 리스트 형태이므로 첫 번째 요소를 사용
 
     # 펫 정보를 가져옵니다.
-    pet_info = get_pet_info(user_id)
-
-    # 'created_at' 값을 템플릿으로 전달합니다.
-    created_at = user_info['created_at']
-
-    # 등록/수정 버튼이 눌렸을 때의 처리
-    if request.method == 'POST':
-        pet_name = request.form['dog_name']
-        year = request.form['year']
-        month = request.form['month']
-        day = request.form['day']
-
-        # 날짜 구성 요소를 하나의 문자열로 결합
-        created_at = f"{year}-{month}-{day}"
-
-        print(f"Pet Name: {pet_name}")
-        print(f"Created At: {created_at}")
-
-        # 데이터베이스에 등록 또는 수정
-        update_pet_info(user_id, pet_name, created_at)
-
-        # 업데이트된 펫 정보 다시 가져오기
-        pet_info = get_pet_info(user_id)
-
-        print(f"Updated Pet Info: {pet_info}")
+    pet_info = pet_info[0]
 
     return render_template('profile.html', user_id=user_info['id'], email=user_info['email'],
-                           nickname=user_info['nick'], phone=user_info['phone'],  pet_info=pet_info, created_at=created_at)
+                           nickname=user_info['nick'], phone=user_info['phone'],  pet_info=pet_info)
+
+from flask import flash
 
 
 @app.route('/update_profile', methods=['POST'])
@@ -383,52 +286,6 @@ def update_profile():
     return redirect(url_for('home'))
 
 
-def generate_date_labels():
-    # 현재 날짜를 기준으로 일주일 동안의 날짜 목록 생성
-    today = datetime.today()
-    date_labels = [(today - timedelta(days=i)).strftime('%Y.%m.%d') for i in range(6, -1, -1)]
-    return date_labels
-
-def extract_date_from_datetime(datetime_obj):
-    # 날짜 부분만을 가져오는 함수
-    return datetime_obj.date()
-
-def calculate_total_result(user_id, date):
-    # 각 날짜에 대해 voice_result 값을 더하여 데이터 생성
-    pet_idx = get_pet_idx(user_id)
-
-    sql = """
-        SELECT SUM(t_voice.voice_result) as total_result
-        FROM t_voice
-        JOIN t_pet ON t_voice.pet_idx = t_pet.pet_idx
-        WHERE t_pet.pet_idx = %(pet_idx)s
-        AND DATE(t_voice.result_at) = %(date)s
-    """
-
-    cursor = mysql.cursor()
-    cursor.execute(sql, {'pet_idx': pet_idx, 'date': date})
-    result = cursor.fetchone()
-    cursor.close()
-
-    return result['total_result'] if result and result['total_result'] else 0
-
-@app.route('/api/bar_chart', methods=['GET'])
-def generate_bar_chart_data():
-    user_id = session.get('login_user')
-
-    if not user_id:
-        return jsonify({'error': '로그인이 필요합니다.'}), 401
-
-    # 현재 날짜를 기준으로 일주일 동안의 날짜 목록 생성
-    labels = generate_date_labels()
-
-    # 각 날짜에 대해 voice_result 값을 더하여 데이터 생성
-    data = [calculate_total_result(user_id, date) for date in labels]
-
-    # JSON 형식으로 반환
-    return jsonify({'labels': labels, 'data': data})
-
-
 
 @app.route('/policy')
 def policy():
@@ -438,14 +295,55 @@ def policy():
 def termsofuse():
     return render_template('termsofuse.html')
 
+# @app.route('/data', methods=['GET','POST'])
+# def receive_data():
+#     #data = request.json
+#     data = request.get_json()
+#     if data is None:
+#         data = json.loads(request.data.decode('utf-8'))
+    # 값이 2 이상인 경우에만 IFTTT로 알림 보내기
+    if isinstance(data, dict) and 'value' in data and data['value'] > 0:
+        try:
+            # 데이터베이스에 연결
+            connection = pymysql.connect(host="project-db-stu3.smhrd.com", 
+                                        db="Insa4_IOTB_final_4", 
+                                        user="Insa4_IOTB_final_4", 
+                                        password="aischool4", 
+                                        port=3307,
+                                        cursorclass=pymysql.cursors.DictCursor)
+            with connection:
+                with connection.cursor() as cursor:
+                    # SQL 업데이트 쿼리
+                    query = "UPDATE t_voice SET voice_result = 1 WHERE voice_idx = %s"
+                    
+                    # SQL 명령 실행
+                    cursor.execute(query, (data['voice_idx']))
+                    
+                    # 변경 사항 커밋
+                    connection.commit()
+                    
+                # 연결 종료
+                connection.close()
+
+            send_notification(data['voice_idx'])
+            return {'success':'Notification sent'}
+        except Exception as e:
+                return {'fail': f'오류: {e}'}
+    else:
+        return {'fail':'Notification no sent'}
+
+def send_notification(voice_idx):
+    pet_name = DQL("select pet_name from t_pet where pet_idx = (select pet_idx from t_voice where voice_idx=%s)", (voice_idx))
+    # IFTTT로 POST 요청 보내기
+    data1 = {pet_name: 'whining'}
+    try:
+        response = requests.post(ifttt_webhook_url, json=data1)
+        response.raise_for_status()  # 나쁜 응답에 대해 HTTPError 발생
+        logger.info("알림이 성공적으로 전송되었습니다.")
+    except requests.exceptions.HTTPError as errh:
+        logger.error(f"HTTP 오류: {errh}")
+    except requests.exceptions.RequestException as err:
+        logger.error(f"요청 예외: {err}")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-
-
-
-
-
-
-
